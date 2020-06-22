19dd5cbe4f574342b7fa83e3524df8a3e4adff8d
hlkdec <on.song@bk.ru>
2020-06-22 19:16:58 +0300

    release

---
+++ script/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		// centerPadding: '15px',
+		infinite: true,
+        slidesToShow: 4,
+        rows:1,
+        slidesToScroll: 1,
+        // autoplay:true,
+        autoplaySpeed:1000,
+        responsive:[{
+        	breakpoint: 1041,
+        	settings:{
+        		slidesToShow:3,
+        		slidesToScroll: 1
+        	}
+        },{
+        	breakpoint:787,
+        	settings:{
+        		slidesToShow:2,
+        		slidesToScroll: 1
+        	}
+        },{
+        	breakpoint:550,
+        	settings:{
+        		slidesToShow:1,
+        		slidesToScroll:1
+        	}
+        }]
+	});
+	$('.preslider').slick({
+		infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        // autoplay:true,
+        autoplaySpeed:5000,
+        fade:true,
+        speed:600,
+        dots:true,
+        
+	})
+});
+$('.dDMenu_icon').on('click',function(){
+	$('.nav').slideToggle();
+});
+
